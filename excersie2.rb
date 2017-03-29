634d3d16c0e109969792c0ca39fc86dd0bc9b5a9
codonculous <feidameng01@gmail.com>
2017-03-28 21:37:07 -0400

    create excersie2

---
+++ excersie2.rb
@@ -0,0 +1,46 @@
+# Print out the last element of the array of your favourite colours.
+# Note: this should work for an array of any size!
+# Add a new city to the hash of cities and population.
+# Reverse the array of coin flips and save it.
+# Print out the population of one of the cities.
+# Print out a sentence about each item in the array of performing artists. For example:
+# I think Pearl Jam is great.
+# I think Lady Gaga is great.
+# I think Pink Floyd is great.
+
+
+# create arraies
+fav_colors = ["red", "yellow", "blue", "green"]
+ages = [800, 500, 1000]
+coin_flip_head = [true, false, false, true, true]
+fav_artist = ["Eminem", "Snoop Dog", "50 Cents"]
+fav_colors_symbol = [:red, :yellow, :blue, :green]
+
+#create hash
+dictionary = {
+
+  trumpet:"a brass musical instrument with a flared bell and a bright, penetrating tone.",
+  cloud:"a network of remote servers hosted on the Internet and used to store, manage, and process data in place of local servers or personal computer.",
+  hack:"use a computer to gain unauthorized access to data in a system."
+
+}
+
+fav_movie = {
+
+  starwars: 1977,
+  godfather: 1972,
+  jasonbourne:2007
+
+}
+
+cities = {
+  beijing:21.5*10**6,
+  shanghai: 24.15*10**6,
+  toronto:2.615*10**6
+}
+
+ages = {
+  feida:500,
+  x:600,
+  y:1000
+}
