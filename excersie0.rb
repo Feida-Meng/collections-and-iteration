f161b546270c07a0fce1a6495c2bec065e884a4f
codonculous <feidameng01@gmail.com>
2017-03-28 21:22:26 -0400

    Create excersie0.rb and create arrays and hashs

---
+++ excersie0.rb
@@ -0,0 +1,49 @@
+# Create an array that contains the given information:
+#
+# your favourite colours
+# the age of you and your siblings/cousins/friends
+# flip a coin 5 times and record whether or not the result was 'heads'
+# your three favourite performing artists
+# your favourite colours again (this time as symbols instead of strings)
+# Create a hash that contains the given information:
+#
+# three words and their definitions
+# your favourite movie names and their year of creation
+# three cities of the world and their population
+# the names of your siblings/cousins/friends and their ages
+
+# create arraies
+fav_colors = ["red", "yellow", "blue", "green"]
+ages = [800, 500, 1000]
+coin_flip_head = [true, false, false, true, true]
+fav_artist = ["Eminem", "Snoop Dog", "50 Cents"]
+fav_colors_symbol = [:red, :yellow, :blue, :green]
+
+#create hash
+dictionary = {
+
+  trumpet:"a brass musical instrument with a flared bell and a bright, penetrating tone.",
+  cloud:"a network of remote servers hosted on the Internet and used to store, manage, and process data in place of local servers or personal computer.",
+  hack:"use a computer to gain unauthorized access to data in a system."
+
+}
+
+fav_movie = {
+
+  starwars: 1977,
+  godfather: 1972,
+  jasonbourne:2007
+
+}
+
+cities = {
+  beijing:21.5*10**6,
+  shanghai: 24.15*10**6,
+  toronto:2.615*10**6
+}
+
+ages = {
+  feida:500,
+  x:600,
+  y:1000
+}
