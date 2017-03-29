1d0775dff586f517d37367c7509366b63865fea5
codonculous <feidameng01@gmail.com>
2017-03-28 21:34:30 -0400

    excersie1 finished

--- excersie1.rb
+++ excersie1.rb
@@ -1,26 +1,22 @@
-# Print out the array of coin flips.
-# Print out the first element of the array of your favourite colours.
-# Output the sorted version of the array of your friends and family members' ages.
-#
-# Add a new baby (0 years old) to the array of your family's ages.
-#
-# Using the hash of movie information, access and print the year of one of the movies.
-
-
 fav_colors = ["red", "yellow", "blue", "green"]
 ages = [800, 500, 1000]
 coin_flip_head = [true, false, false, true, true]
 fav_artist = ["Eminem", "Snoop Dog", "50 Cents"]
 fav_colors_symbol = [:red, :yellow, :blue, :green]
 
-#create hash
-dictionary = {
+# Print out the array of coin flips.
+puts coin_flip_head
 
-  trumpet:"a brass musical instrument with a flared bell and a bright, penetrating tone.",
-  cloud:"a network of remote servers hosted on the Internet and used to store, manage, and process data in place of local servers or personal computer.",
-  hack:"use a computer to gain unauthorized access to data in a system."
+# Print out the first element of the array of your favourite colours.
+puts fav_colors[0]
 
-}
+# Output the sorted version of the array of your friends and family members' ages.
+puts ages.sort
+
+# Add a new baby (0 years old) to the array of your family's ages.
+ages.push(0)
+
+# Using the hash of movie information, access and print the year of one of the movies.
 
 fav_movie = {
 
@@ -30,14 +26,4 @@ fav_movie = {
 
 }
 
-cities = {
-  beijing:21.5*10**6,
-  shanghai: 24.15*10**6,
-  toronto:2.615*10**6
-}
-
-ages = {
-  feida:500,
-  x:600,
-  y:1000
-}
+puts fav_movie[:starwars]
