52cb64518e22b123fbf305ab6bb176facbc1bef0
codonculous <feidameng01@gmail.com>
2017-03-28 21:25:40 -0400

    create excersie1.rb

---
+++ excersie1.rb
@@ -0,0 +1,43 @@
+# Print out the array of coin flips.
+# Print out the first element of the array of your favourite colours.
+# Output the sorted version of the array of your friends and family members' ages.
+#
+# Add a new baby (0 years old) to the array of your family's ages.
+#
+# Using the hash of movie information, access and print the year of one of the movies.
+
+
+fav_colors = ["red", "yellow", "blue", "green"]
+ages = [800, 500, 1000]
+coin_flip_head = [true, false, false, true, true]
+fav_artist = ["Eminem", "Snoop Dog", "50 Cents"]
+fav_colors_symbol = [:red, :yellow, :blue, :green]
+
+#create hash
+dictionary = {
+
+  trumpet:"a brass musical instrument with a flared bell and a bright, penetrating tone.",
+  cloud:"a network of remote servers hosted on the Internet and used to store, manage, and process data in place of local servers or personal computer.",
+  hack:"use a computer to gain unauthorized access to data in a system."
+
+}
+
+fav_movie = {
+
+  starwars: 1977,
+  godfather: 1972,
+  jasonbourne:2007
+
+}
+
+cities = {
+  beijing:21.5*10**6,
+  shanghai: 24.15*10**6,
+  toronto:2.615*10**6
+}
+
+ages = {
+  feida:500,
+  x:600,
+  y:1000
+}
